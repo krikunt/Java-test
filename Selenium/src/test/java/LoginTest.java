ffc77d2f7fe3053b8c6f2f7a71af96156808e760
krikunt <krikunt362@gmail.com>
2025-10-28 13:37:02 +0200

    task10

--- Selenium/src/test/java/LoginTest.java
+++ Selenium/src/test/java/LoginTest.java
@@ -1,8 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import java.util.List;
 
 public class LoginTest {
     WebDriver driver;
@@ -11,17 +14,37 @@ public class LoginTest {
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
     }
 
     @Test
     public void openSauceDemo() {
-        driver.get("https://www.saucedemo.com/");
         String pageTitle = driver.getTitle();
 
         if (pageTitle.contains("Swag Labs")) {
             System.out.println("Перевірку пройдено: заголовок містить \"Swag Labs\"");
         } else {
             System.out.println("Перевірку не пройдено: заголовок не містить \"Swag Labs\"");
+        }
+    }
+
+    @Test
+    public void checkElementsPresence() {
+
+        if (!driver.findElements(By.id("login-button")).isEmpty()) {
+            System.out.println("Кнопку Login знайдено");
+        }
+
+        if (!driver.findElements(By.className("login_logo")).isEmpty()) {
+            System.out.println("Заголовок Swag Labs знайдено");
+        }
+
+        if (!driver.findElements(By.xpath("//input[contains(@class, 'form_input')]")).isEmpty()) {
+            System.out.println("Кнопка Username знайдено");
+        }
+
+        if (!driver.findElements(By.cssSelector(".login_logo")).isEmpty()){
+            System.out.println("Pаголовок Swag Labs знайдено");
         }
     }
 
---
+++ Selenium/src/test/java/OpenSauceDemo.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenSauceDemo {
+    public static void main(String[] args) {
+      WebDriver driver = new ChromeDriver();
+
+      driver.get("https://www.saucedemo.com/");
+
+      driver.manage().window().maximize();
+    }
+}
