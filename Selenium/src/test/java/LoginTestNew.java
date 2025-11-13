f52c4d16711d1ad85f74cce3a83ee9b99a7b323f
krikunt <krikunt362@gmail.com>
2025-11-13 18:01:01 +0200

    task 13

--- Selenium/src/test/java/LoginTestNew.java
+++ Selenium/src/test/java/LoginTestNew.java
@@ -1,35 +1,35 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+import pages.LoginPage;
 
 public class LoginTestNew {
 
     WebDriver driver;
+    LoginPage loginPage;
 
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
+        loginPage = new LoginPage(driver);
     }
 
     @Test
     public void checkLoginPage() {
         String title = driver.getTitle();
-        org.testng.Assert.assertTrue(title.contains("Swag Labs"), "Назва сторінки не містить тексту 'Swag Labs'");
+        org.testng.Assert.assertTrue(title.contains("Swag Labs"),
+                "Назва сторінки не містить тексту 'Swag Labs'");
 
         SoftAssert softAssert = new SoftAssert();
 
-        WebElement usernameField = driver.findElement(By.id("user-name"));
-        softAssert.assertTrue(usernameField.isDisplayed(),"Поле логіну не відображається");
-
-        WebElement passwordField = driver.findElement(By.id("password"));
-        softAssert.assertTrue(passwordField.isDisplayed(), "Поле паролю не відображається");
+        softAssert.assertTrue(loginPage.isUsernameVisible(), "Поле логіну не відображається");
+        softAssert.assertTrue(loginPage.isPasswordVisible(), "Поле паролю не відображається");
+        softAssert.assertTrue(loginPage.isLoginButtonVisible(), "Кнопка входу не відображається");
 
         softAssert.assertAll();
     }
---
+++ Selenium/src/test/java/pages/LoginPage.java
@@ -0,0 +1,52 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "user-name")
+    private WebElement usernameField;
+
+    @FindBy(id = "password")
+    private WebElement passwordField;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    public boolean isUsernameVisible() {
+        return usernameField.isDisplayed();
+    }
+
+    public boolean isPasswordVisible() {
+        return passwordField.isDisplayed();
+    }
+
+    public boolean isLoginButtonVisible() {
+        return loginButton.isDisplayed();
+    }
+
+    public void enterUsername(String username) {
+        usernameField.sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        passwordField.sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        loginButton.click();
+    }
+}
+
+
+
